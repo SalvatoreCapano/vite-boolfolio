0016788cf57dc203ec5939cb6ad8c533aa63ec3f
Salvatore Capano <capano_salvatore@libero.it>
2023-03-29 16:31:43 +0200

    Bonus Completato

--- src/App.vue
+++ src/App.vue
@@ -13,17 +13,37 @@ export default {
     return {
       host: 'http://127.0.0.1:8000',
       data: null,
+      page: 1,
+      lastPage: 1
     }
   },
   methods: {
     getProjects() {
-      // console.log('Richiesta progetti...')
-      axios.get(`${this.host}/api/projects`)
+      // console.log('Richiesta progetti...');
+      axios.get(`${this.host}/api/projects`, {
+        params: {
+          page: this.page
+        }
+      })
         .then((response) => {
-          console.log ('Dati risposta:', response.data);
-          this.data = response.data.projects;
+          console.log('Dati risposta:', response.data);
+          this.data = response.data.projects; //Senza paginazione
+          this.data = response.data.projects.data; //Con paginazione
+          this.lastPage = response.data.projects.last_page;
         });
       // console.log('Risposta ottenuta.')
+    },
+    setPage(direction) {
+
+      if (direction == 'prev' && this.page > 1) {
+        this.page--;
+        this.getProjects();
+      }
+      else if (direction == 'next' && this.page < this.lastPage) {
+        this.page++;
+        this.getProjects();
+      }
+
     }
   },
   created() {
@@ -37,9 +57,24 @@ export default {
   <div class="container">
     <h1>Projects</h1>
     <div class="cardsContainer" v-if="this.data != null">
-      <ProjectCard v-for="elem in this.data" :project="elem"/>
+      <ProjectCard v-for="elem in this.data" :project="elem" />
     </div>
+
     <p v-else>Caricamento Progetti...</p>
+
+    <div class="controls">
+
+      <button @click="this.setPage('prev')">
+        <i class="bi bi-chevron-left"></i>
+      </button>
+
+      <span>Page {{ this.page }}</span>
+
+      <button @click="this.setPage('next')">
+        <i class="bi bi-chevron-right"></i>
+      </button>
+
+    </div>
   </div>
 </template>
 
@@ -58,9 +93,22 @@ export default {
   align-items: center;
   flex-wrap: wrap;
   gap: 1rem;
+
+  margin-bottom: 2rem;
 }
 
 p {
   color: white;
+}
+
+.controls {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+
+  span {
+    color: white;
+  }
 }
 </style>
